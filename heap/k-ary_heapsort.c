e6cede3f24a35df5c752387002b3e99f152fefe8
Bishnu Thakur <bishnuthakur284@gmail.com>
2022-04-28 14:15:58 +0530

    K-ary heap sort added

--- heap/k-ary_heapsort.c
+++ heap/k-ary_heapsort.c
@@ -1,69 +1,77 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define child k*index+i 
-int heap_sort(int *,int);
-void max_heap(int *,int ,int);
-void swap(int *,int *);
+#include <stdio.h>
+#include <stdlib.h>
+#define child k *index + i
+void heapsort(int *, int);
+void max_heap(int *, int, int);
+void swap(int *, int *);
+void heapify(int *, int);
 int k;
 int main()
 {
-	int *arr,n;
+	int *arr, n;
+
 	printf("Enter the value of k: ");
-	scanf("%d",&k);
+	scanf("%d", &k);
+
 	printf("Enter no. of elements: ");
-	scanf("%d",&n);
-	arr=(int *)calloc(n,sizeof(int));
-	printf("Enter %d elments;\n",n);
-	for(int i=0;i<n;i++)
-		scanf("%d",&arr[i]);
-	max_heap(arr,n-1,(n-2)/k);
-	printf("Elements in the %d-ary are: ",k);
-	for(int i=0;i<n;i++)
-		printf("%d ",arr[i]);
-	heapsort(arr,n);
+	scanf("%d", &n);
+
+	arr = (int *)calloc(n, sizeof(int));
+
+	printf("Enter %d elments;\n", n);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+	// max_heap(arr,n-1,(n-2)/k);
+	heapify(arr, n);
+	printf("Elements in the %d-ary are: ", k);
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+	heapsort(arr, n);
 	printf("\nSorted elements are: ");
-	for(int i=0;i<n;i++)
-		printf("%d ",arr[i]);
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
 	printf("\n");
 	return 0;
 }
 
 void max_heap(int *arr, int size, int index)
 {
-	if(index>=0)
+	if (index >= 0)
 	{
-		int i=1;
-		while(i<=k && child<=size)
+		int i = 1;
+		while (i <= k && child <= size)
 		{
-			if(arr[child]>arr[index])
+			if (arr[child] > arr[index])
 			{
-				swap(&arr[child],&arr[index]);
-				max_heap(arr,size,child);
+				swap(&arr[child], &arr[index]);
+				max_heap(arr, size, child);
 			}
 			i++;
 		}
-		//max_heap(arr,size,index-1);
+		// max_heap(arr,size,index-1);
 	}
 }
 
-void heapsort(int *arr,int n)
+void heapsort(int *arr, int n)
 {
-	for(int i=n-1;i>0;i--)
+	for (int i = n - 1; i > 0; i--)
 	{
-		swap(&arr[0],&arr[i]);
-		max_heap(arr,i-1,0);
+		swap(&arr[0], &arr[i]);
+		max_heap(arr, i - 1, 0);
 	}
 }
 
-void swap(int *a,int *b)
+void swap(int *a, int *b)
 {
-	*a^=*b;
-	*b^=*a;
-	*a^=*b;
+	*a ^= *b;
+	*b ^= *a;
+	*a ^= *b;
 }
 
-void heapify(int *arr,int size)
+void heapify(int *arr, int size)
 {
-	for(int i=(n-2)/k;i>=0;i++)
+	for (int i = (size - 2) / k; i >= 0; i--)
 	{
-		max_heap(arr,
+		max_heap(arr, size - 1, i);
+	}
+}
